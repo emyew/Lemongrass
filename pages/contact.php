8b7b8cc711644b5dd384eb4ee8f600806a1481a3
Emily Yew <emyew2@gmail.com>
2016-03-03 13:29:04 -0800

    Update contact php and html, add thank you page

--- pages/contact.php
+++ pages/contact.php
@@ -1,39 +1,58 @@
 <?php
-	if(isset($_POST["submit"])) {
-		$firstName=$_POST['firstName'];
-		$lastName=$_POST['lastName'];
-		$email=$_POST['email'];
-		$message=$_POST['message'];
 
-		$from = 'Lemongrass Contact Form';
-		$to = 'emyew2@gmail.com';
-		$subject = 'Feedback for Lemongrass';
+$first_name = $_POST['firstName'];
+$last_name = $_POST['lastName'];
+$visitor_email = $_POST['email'];
+$message = $_POST['message'];
 
-		$body = "From: $firstName\n Email: $email\n Message:\n $message";
-
-
-	if (!$_POST['firstName']) {
-		$errFirstName = 'Please enter your first name.';
-	}
-
-	if (!$_POST['lastName']) {
-		$errLastName = 'Please enter your last name.';
-	}
-
-	if (!$_POST['email'] || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
-    	$errEmail = 'Please enter a valid email address';
-	}
-
-	if (!$_POST['message']) {
-		$errMessage = 'Please enter your message.';
-	}
-
-	if (!$errFirstName && $errLastName && !$errEmail && !$errMessage) {
-    	if (@mail ($to, $subject, $body, $from)) {
-        	$result='<div class="alert alert-success">Thank You! I will be in touch</div>';
-    	} else {
-        	$result='<div class="alert alert-danger">Sorry there was an error sending your message. Please try again later</div>';
-    	}
-	}
+//Validate first
+if(empty($message)||empty($visitor_email)||empty($first_name)||empty($last_name))
+{
+    echo "Name, email, and feedback are mandatory!";
+    exit;
 }
+
+if(IsInjected($visitor_email))
+{
+    echo "Bad email! Please try again!";
+    exit;
+}
+
+$email_from = $visitor_email;//<== update the email address
+$email_subject = "Lemongrass Feedback Form Submission from $first_name $last_name";
+$email_body = "You have received a new message from the user $first_name $last_name at $visitor_email.\n\n".
+    "Here is the message:\n $message\n\n".
+
+$to = "emyew2@gmail.com";//<== update the email address
+$headers = "From: $email_from \r\n";
+$headers .= "Reply-To: $visitor_email \r\n";
+//Send the email!
+mail($to,$email_subject,$email_body,$headers);
+//done. redirect to thank-you page.
+header('Location: thank-you.html');
+
+
+// Function to validate against any email injection attempts
+function IsInjected($str)
+{
+  $injections = array('(\n+)',
+              '(\r+)',
+              '(\t+)',
+              '(%0A+)',
+              '(%0D+)',
+              '(%08+)',
+              '(%09+)'
+              );
+  $inject = join('|', $injections);
+  $inject = "/$inject/i";
+  if(preg_match($inject,$str))
+    {
+    return true;
+  }
+  else
+    {
+    return false;
+  }
+}
+
 ?>
